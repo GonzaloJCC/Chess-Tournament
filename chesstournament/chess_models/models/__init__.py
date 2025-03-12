e784c769bff58c3fab716af990670656f1deafa7
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-12 23:01:49 +0100

    Created the model folder instead of the model file

---
+++ chesstournament/chess_models/models/__init__.py
@@ -0,0 +1,4 @@
+# Import classes like
+# 	<<from .file_with_modelX import modelX # noqa F104>>
+# It is important to add "# noqa F401" at the end of the import to informs
+# flake8 to ignore the fact that the model is never use in this file
