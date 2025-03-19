cef7f487ffbc95c31a9e296d02000fac109700bc
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-19 19:18:01 +0100

    Added more models on the module init file

--- chesstournament/chess_models/models/__init__.py
+++ chesstournament/chess_models/models/__init__.py
@@ -3,7 +3,9 @@
 # It is important to add "# noqa F401" at the end of the import to informs
 # flake8 to ignore the fact that the model is never use in this file
 
+from .constants import *
 from .player import Player # noqa F104
 from .other_models import Referee
 from .tournament import Tournament, RankingSystemClass
 from .round import Round
+from .game import Game
