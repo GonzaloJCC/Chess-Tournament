d15867bcd14207c31ce05aa408a18b4e0da4d9af
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-29 19:03:28 +0100

    Implemented the Referee view set

--- chesstournament/api/urls.py
+++ chesstournament/api/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from .views import RefereeViewSet
 
 # Define the router and register the viewsets
 router = DefaultRouter()
-# router.register(r'referees', RefereeViewSet)
+router.register(r'referees', RefereeViewSet)
 # router.register(r'players', PlayerViewSet)
 # router.register(r'games', GameViewSet)
 # router.register(r'tournaments', TournamentViewSet)
--- chesstournament/api/views.py
+++ chesstournament/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from chess_models.models import Referee
+from chess_models.serializers import RefereeSerializer
 
-# Create your views here.
+class RefereeViewSet(viewsets.ModelViewSet):
+	queryset = Referee.objects.all()
+	serializer_class = RefereeSerializer
