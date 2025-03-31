7bc5a4e66d72fed34df8843c5f882fc8464d97d5
Luiz-Pastor <luigpacr5@gmail.com>
2025-03-31 17:45:32 +0200

    Specified the imports

--- chesstournament/api/views.py
+++ chesstournament/api/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render
-from rest_framework import viewsets, pagination, status
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
-from djoser import views
+from djoser.views import UserViewSet
 
 from chess_models.models import (
 	Referee,
@@ -24,7 +26,7 @@ from chess_models.serializers import (
 # NOTE: Pagination class #
 ##########################
 
-class TournamnetPagination(pagination.PageNumberPagination):
+class TournamnetPagination(PageNumberPagination):
 	page_size = 10
 	page_size_query_param = 'page_size'
 	max_page_size = 100
@@ -33,15 +35,15 @@ class TournamnetPagination(pagination.PageNumberPagination):
 ########################
 # NOTE: Models Classes #
 ########################
-class RefereeViewSet(viewsets.ModelViewSet):
+class RefereeViewSet(ModelViewSet):
 	queryset = Referee.objects.all()
 	serializer_class = RefereeSerializer
 
-class PlayerViewSet(viewsets.ModelViewSet):
+class PlayerViewSet(ModelViewSet):
 	queryset = Player.objects.all()
 	serializer_class = PlayerSerializer
 
-class GameViewSet(viewsets.ModelViewSet):
+class GameViewSet(ModelViewSet):
 	queryset = Game.objects.all()
 	serializer_class = GameSerializer
 
@@ -60,8 +62,7 @@ class GameViewSet(viewsets.ModelViewSet):
 		else:
 			raise PermissionDenied("This game has already finished and cannot be updated.")
 
-
-class TournamentViewSet(viewsets.ModelViewSet):
+class TournamentViewSet(ModelViewSet):
 	queryset = Tournament.objects.all()
 	serializer_class = TournamentSerializer
 	pagination_class = TournamnetPagination
@@ -72,12 +73,11 @@ class TournamentViewSet(viewsets.ModelViewSet):
 			return [AllowAny()]
 		return super().get_permissions()
 
-
-class RoundViewSet(viewsets.ModelViewSet):
+class RoundViewSet(ModelViewSet):
 	queryset = Round.objects.all()
 	serializer_class = RoundSerializer
 
-class CustomUserViewSet(views.UserViewSet):
+class CustomUserViewSet(UserViewSet):
 	def create(self, request, *args, **kwargs):
 		return Response(
 			{'error': 'Method not allowed'},
