b6ff7ec903d815c23d5808792b801e7bb3c39213
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-05 23:50:30 +0200

    Created the script to init the dependencies and env on render

---
+++ chesstournament/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Install the dependencies
+pip install -r requirements.txt
+
+# Collect the static files
+python manage.py collectstatic --no-input
+
+# Miagrate the database data
+python3 manage.py makemigrations
+python manage.py migrate
+
+# Create the super user
+# python manage.py createsu
+make create_super_user
