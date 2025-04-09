9953abff2a250607e5cf593f13755666441d38ea
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-09 10:01:39 +0200

    Implemented the routes on the app nav

--- chesstournament-client/src/components/AppNav.vue
+++ chesstournament-client/src/components/AppNav.vue
@@ -1,27 +1,24 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">Chess-T-DB</a>
+    <a class="navbar-brand"><router-link class="navbar-brand" to="/">Chess-T-DB</router-link></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
+          <a class="nav-link"><router-link class="nav-link" to="/">Home</router-link></a>
         </li>
         <li class="nav-item">
-          <!-- TODO : Add route -->
           <!-- TODO : Add disable when the user is logged -->
-          <a class="nav-link" href="#">Admin Log-In</a>
+          <a class="nav-link"><router-link class="nav-link" to="/log-in">Admin Log-In</router-link></a>
         </li>
         <li class="nav-item">
-          <!-- TODO : Add route -->
           <!-- TODO : Add disable when the user is not logged -->
-          <a class="nav-link" href="#">Log-Out</a>
+          <a class="nav-link"><router-link class="nav-link" to="/log-out">Log-Out</router-link></a>
         </li>
         <li class="nav-item">
-          <!-- TODO : Add route -->
-          <a class="nav-link" href="#">FAQ</a>
+          <a class="nav-link"><router-link class="nav-link" to="/faq">FAQ</router-link></a>
         </li>
       </ul>
     </div>
@@ -33,6 +30,6 @@
 
 <style scoped>
 	nav a {
-	padding-left: 10px;
+	  padding-left: 10px;
 	}
 </style>
