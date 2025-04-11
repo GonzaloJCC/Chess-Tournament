0298195e9611a6b7d0eed709ac49292a40dcf543
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-11 11:24:58 +0200

    Init the login form component. Only lefts the send of the data to the parent

---
+++ chesstournament-client/src/components/LoginForm.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">
+      Login
+    </h1>
+
+    <form @submit.prevent="handleLogin">
+      <!-- Error message -->
+      <div
+        v-if="error.value"
+        class="alert alert-danger text-center"
+        role="alert"
+      >
+        {{ error.msg }}
+      </div>
+
+      <!-- Username input --> 
+      <div class="mb-3">
+        <input
+          v-model="user.username"
+          type="text"
+          placeholder="username"
+          class="form-control rounded-pill border-0 shadow-sm px-4"
+          required
+        >
+      </div>
+
+      <!-- Password input -->
+      <div class="mb-3">
+        <input
+          v-model="user.password"
+          type="password"
+          placeholder="password"
+          required
+          class="form-control rounded-pill border-0 shadow-sm px-4"
+        >
+      </div>
+
+      <!-- Submit button -->
+      <div class="d-grid gap-2 mt-2">
+        <button
+          type="submit"
+          class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref, computed } from 'vue';
+  const APIURL = import.meta.env.VITE_APIURL;
+
+  /* NOTE: Object where the data will be saved */
+  const user = ref({
+    username: '',
+    password: ''
+  })
+
+  /* NOTE: Computed properties to validate the inputs */
+  const usernameValid = computed(() => {
+    return user.value.username.length > 0;
+  });
+
+  const passwordValid = computed(() => {
+    return user.value.password.length > 0;
+  });
+
+  /* NOTE: Possible errors */
+  const error = ref({
+    value: false,
+    msg: '',
+  
+    /* Function to set an error */
+    set(msg) {
+      this.value = true;
+      this.msg = msg;
+    },
+
+    /* Function to reset the error state */
+    reset() {
+      this.value = false;
+      this.msg = '';
+    }
+  });
+
+  /* NOTE: Function called when the form is submitted */
+  const handleLogin = async () => {
+    /* Reset state */
+    error.value.reset()
+
+    /* Check the inputs */
+    if (!usernameValid.value || !passwordValid.value) {
+      error.value.set('All the fields has to be filled');
+      return;
+    }
+
+    /* The data has been provided: request to API */
+    try {
+      const response = await fetch(`${APIURL}/api/v1/token/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: user.value.username,
+          password: user.value.password
+        })
+      });
+
+      if (!response.ok) throw new Error('Invalid username or password')
+      
+      /* TODO: Send the information to the parent */
+
+    } catch(e)
+    {
+      error.value.set(`Error: ${e.message}`);
+      return ;
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    width: 50%;
+    border: 2px solid #d6d7d0;
+    border-radius: 5px;
+  }
+</style>
