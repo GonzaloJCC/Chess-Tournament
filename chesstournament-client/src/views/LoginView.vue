7a875ecba8122bc18f662cefe625968cf2b1cc6a
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-11 11:49:29 +0200

    Implemented the Login View

--- chesstournament-client/src/views/LoginView.vue
+++ chesstournament-client/src/views/LoginView.vue
@@ -1,8 +1,22 @@
 <template>
-  <p>Login View</p>
+  <LoginForm
+    @confirm-form="handleLogin"
+  />
 </template>
 
 <script setup>
+  import LoginForm from '@/components/LoginForm.vue'
+  import { useTokenStore } from '@/stores/token';
+  import { useRouter } from 'vue-router';
+
+  const tokenStore = useTokenStore();
+  const router = useRouter();
+
+  /* NOTE: Function to log in the user, with the API data */
+  const handleLogin = (data) => {
+    tokenStore.setToken(data.auth_token);
+    router.push('/')
+  }
 </script>
 
 <style scope>
