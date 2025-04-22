0a101155cd92c78fcc446904e65c01451f96a221
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-22 22:54:02 +0200

    Added the first FAQ question

--- chesstournament-client/src/views/FAQView.vue
+++ chesstournament-client/src/views/FAQView.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="container my-5">
-  <h2 class="mb-4">Preguntas Frecuentes</h2>
+  <h2 class="mb-4">FAQ</h2>
   <div class="accordion" id="faqAccordion">
     <!-- FAQ Item 1 -->
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingOne">
         <button class="accordion-button collapsed shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-          Example 1
+          How can players update the result of a game?
         </button>
       </h2>
       <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#faqAccordion">
         <div class="accordion-body">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus leo, sollicitudin ut pulvinar a, faucibus non nunc.
-          Ut porta nibh sed nunc vehicula, non ornare turpis rutrum. Integer ac urna neque. Maecenas et nulla sit amet ipsum sagittis
-          porta eu eget nisi. Sed accumsan tincidunt mauris non tempor. Suspendisse finibus tortor vel libero blandit laoreet. Donec
-          accumsan, mi eget tempus elementum, tortor nisl mattis arcu, et pretium mi neque ac sem. Pellentesque posuere velit a ante
-          semper tempus.
+          <p>
+            Go to the tournament page, open the "pairing/results" tab, provide de lichess game id in the input field (for lichess games)
+            or select the result from the dropdown list (for OTB games) and then click on the 
+            <i class="bi bi-send"></i>
+            icon.
+          </p>
         </div>
       </div>
     </div>
