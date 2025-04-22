69f236403b4abc8b46fe813e73f729947db5fbdb
Luiz-Pastor <luigpacr5@gmail.com>
2025-04-22 22:30:13 +0200

    Designed a simple faq version with the bootstrap collapse tutorial

--- chesstournament-client/src/views/FAQView.vue
+++ chesstournament-client/src/views/FAQView.vue
@@ -1,5 +1,44 @@
 <template>
-  <p>FAQ View</p>
+  <div class="container my-5">
+  <h2 class="mb-4">Preguntas Frecuentes</h2>
+  <div class="accordion" id="faqAccordion">
+    <!-- FAQ Item 1 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingOne">
+        <button class="accordion-button collapsed shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+          Example 1
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#faqAccordion">
+        <div class="accordion-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus leo, sollicitudin ut pulvinar a, faucibus non nunc.
+          Ut porta nibh sed nunc vehicula, non ornare turpis rutrum. Integer ac urna neque. Maecenas et nulla sit amet ipsum sagittis
+          porta eu eget nisi. Sed accumsan tincidunt mauris non tempor. Suspendisse finibus tortor vel libero blandit laoreet. Donec
+          accumsan, mi eget tempus elementum, tortor nisl mattis arcu, et pretium mi neque ac sem. Pellentesque posuere velit a ante
+          semper tempus.
+        </div>
+      </div>
+    </div>
+
+    <!-- FAQ Item 2 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingTwo">
+        <button class="accordion-button collapsed shadow-sm" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Example 2
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faqAccordion">
+        <div class="accordion-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus leo, sollicitudin ut pulvinar a, faucibus non nunc.
+          Ut porta nibh sed nunc vehicula, non ornare turpis rutrum. Integer ac urna neque. Maecenas et nulla sit amet ipsum sagittis
+          porta eu eget nisi. Sed accumsan tincidunt mauris non tempor. Suspendisse finibus tortor vel libero blandit laoreet. Donec
+          accumsan, mi eget tempus elementum, tortor nisl mattis arcu, et pretium mi neque ac sem. Pellentesque posuere velit a ante
+          semper tempus.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script setup>
